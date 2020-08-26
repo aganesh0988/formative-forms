db2567a5b17888c691fb6a55773471030e942654
Jeff <jlgranof@icloud.com>
2020-08-26 16:06:53 -0500

    Phase2-create-user-COMPLETE

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const csrf = require('csurf')
+const cookieParser = require('cookie-parser')
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.set("view engine", "pug");
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+const csrfProtection = csrf({ cookie: true})
 
 const users = [
   {
@@ -17,6 +23,9 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Home Page", users });
 });
 
+app.get("/create", csrfProtection, (req, res) => {
+  res.render("create-user", {title: "Create User", messages: [], csrfToken: req.csrfToken()});
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

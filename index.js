d7104c024b689dc725ffc3cf74b4eb1f66293033
Andrea <aganesh0988@gmail.com>
2020-08-26 18:00:30 -0500

    Phase-3:-Submitting-the-form-COMPLETE

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set("view engine", "pug");
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-const csrfProtection = csrf({ cookie: true})
+const csrfProtection = csrf({ cookie: true })
 
 const users = [
   {
@@ -24,9 +24,47 @@ app.get("/", (req, res) => {
 });
 
 app.get("/create", csrfProtection, (req, res) => {
-  res.render("create-user", {title: "Create User", messages: [], csrfToken: req.csrfToken()});
+  res.render("create-user", { title: "Create User", messages: [], csrfToken: req.csrfToken() });
 })
 
+app.post("/create", csrfProtection, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const errors = [];
+
+  if (!firstName) {
+    errors.push("Please provide a first name.")
+  }
+
+  if (!lastName) {
+    errors.push("Please provide a last name.")
+  }
+
+  if (!email) {
+    errors.push("Please provide an email.")
+  }
+
+  if (!password) {
+    errors.push("Please provide a password.")
+  }
+
+  if (password !== confirmedPassword) {
+    errors.push("The provided values for the password and password confirmation fields did not match.")
+  }
+
+  if (errors.length > 0) {
+    res.render("create-user", { title: "Create User", errors, firstName, lastName, email });
+    return;
+  }
+
+  users.push({
+    id: users.length + 1,
+    firstName,
+    lastName,
+    email
+  });
+  res.redirect("/")
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
